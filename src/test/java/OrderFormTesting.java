29cde0fcb5d76c227b2ec45b05f94681daf233dc
Maria Minor <meverik88tt@gmail.com>
2022-09-02 22:41:18 +0300

    config updating12345

--- src/test/java/OrderFormTesting.java
+++ src/test/java/OrderFormTesting.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.util.List;
 
@@ -22,7 +21,7 @@ public class OrderFormTesting {
     @BeforeAll
     static void setUpAll() {
         WebDriverManager.chromedriver().setup();
-//        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
     }
 
     @BeforeEach
@@ -53,6 +52,19 @@ public class OrderFormTesting {
 
 
     }
-    
+
+    @Test
+    void testCssSelector() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Петров Иван");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79254442233");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_view_extra")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
+
+    }
+
 
 }
