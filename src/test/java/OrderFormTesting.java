ed97ae4b1618597bb5848336d124e44714c70a6a
Maria Minor <meverik88tt@gmail.com>
2022-09-02 23:10:24 +0300

    config updating21

--- src/test/java/OrderFormTesting.java
+++ src/test/java/OrderFormTesting.java
@@ -1,4 +1,46 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//public class OrderFormTesting {
+//
+//
+//
+//    private WebDriver driver;
+//
+//
+//
+//
+//
+//    @BeforeAll
+//    static void setUpAll() {
+//        WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+//    }
+//
+//    @BeforeEach
+//    void setUp() {
+//        driver = new ChromeDriver();
+//    }
+//
+//
+//    @AfterEach
+//    void tearDown() {
+//        driver.quit();
+//        driver = null;
+//    }
+//
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,50 +50,46 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.List;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class OrderFormTesting {
+class ChromeTest {
 
-    private WebDriver driver;
-
-
+    WebDriver driver;
 
     @BeforeAll
-    static void setUpAll() {
+    static void setupAll() {
         WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
     }
 
     @BeforeEach
-    void setUp() {
+    void setup() {
         driver = new ChromeDriver();
     }
 
-
     @AfterEach
-    void tearDown() {
+    void teardown() {
         driver.quit();
-        driver = null;
     }
 
 
 
 
-    @Test
-    void test() {
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Петров Иван");
-        elements.get(1).sendKeys("+79254442233");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button_view_extra")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
+//    @Test
+//    void test() {
+//        driver.get("http://localhost:9999");
+//        List<WebElement> elements = driver.findElements(By.className("input__control"));
+//        elements.get(0).sendKeys("Петров Иван");
+//        elements.get(1).sendKeys("+79254442233");
+//        driver.findElement(By.className("checkbox__box")).click();
+//        driver.findElement(By.className("button_view_extra")).click();
+//        String text = driver.findElement(By.className("paragraph")).getText();
+//        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
 
 
-    }
+//    }
 
     @Test
     void testCssSelector() {
